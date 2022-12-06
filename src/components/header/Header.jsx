f58e2dceb0e09602c962c3e6147a7922a6b75893
Brian <brian.lockerbie@gmail.com>
2022-12-06 14:35:38 -0800

    added react icons

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './header.css'
 import CTA from './CTA'
 import ME from '../../assets/brian-pro-pic.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
@@ -11,6 +12,7 @@ const Header = () => {
         <h1>Brian Lockerbie</h1>
         <h5 className="text-light">Fullstack Developer</h5>
         <CTA/>
+        <HeaderSocials />
 
         <div className="me">
           <img src={ME} alt="me" />
