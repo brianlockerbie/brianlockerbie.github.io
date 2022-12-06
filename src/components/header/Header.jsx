40ad4f87a7f81b78a609e83d900f008fc3dc26ea
Brian <brian.lockerbie@gmail.com>
2022-12-06 14:15:17 -0800

    added function & classNames to header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/brian-pro-pic.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className=".container.header__container">
+        <h5>Hello I'm</h5>
+        <h1>Brian Lockerbie</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA/>
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scrool Down</a>
+      </div>
+    </header>
   )
 }
 
