ba6ebe677c4ad685d2ce131c71ee8c2c764dd019
Brian <brian.lockerbie@gmail.com>
2022-12-06 18:50:50 -0800

    added content & icon to about component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/brian-about.png'
+import {FaAward} from 'react-icons/fa'
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About image" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>2+ Year's</small>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
