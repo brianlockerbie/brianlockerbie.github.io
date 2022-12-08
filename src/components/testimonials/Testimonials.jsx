75c0ea4bc5701bb708183e90f5b5bd65ca58795c
Brian <brian.lockerbie@gmail.com>
2022-12-07 23:58:21 -0800

    added array map to generate testimonials

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -2,6 +2,29 @@ import React from 'react'
 import './testimonials.css'
 import AVTR1 from '../../assets/avatar1.jpg'
 
+const data = [
+  {
+    avatar: AVTR1,
+    name: 'Scott Grove',
+    review: 'Nulla non adipisicing amet ut anim. Ad laborum laboris veniam deserunt eiusmod. Dolor pariatur sit eiusmod adipisicing excepteur duis fugiat laborum occaecat cupidatat sit sint ex.'
+  },
+  {
+    avatar: AVTR1,
+    name: 'Scott Grove',
+    review: 'Nulla non adipisicing amet ut anim. Ad laborum laboris veniam deserunt eiusmod. Dolor pariatur sit eiusmod adipisicing excepteur duis fugiat laborum occaecat cupidatat sit sint ex.'
+  },
+  {
+    avatar: AVTR1,
+    name: 'Scott Grove',
+    review: 'Nulla non adipisicing amet ut anim. Ad laborum laboris veniam deserunt eiusmod. Dolor pariatur sit eiusmod adipisicing excepteur duis fugiat laborum occaecat cupidatat sit sint ex.'
+  },
+  {
+    avatar: AVTR1,
+    name: 'Scott Grove',
+    review: 'Nulla non adipisicing amet ut anim. Ad laborum laboris veniam deserunt eiusmod. Dolor pariatur sit eiusmod adipisicing excepteur duis fugiat laborum occaecat cupidatat sit sint ex.'
+  },
+]
+
 const Testimonials = () => {
   return (
     <section id='testimonials'>
@@ -9,42 +32,19 @@ const Testimonials = () => {
       <h2>Testimonials</h2>
 
       <div className="container testimonials__container">
-        <article className="testimonial">
+        {
+          data.map(({avatar, name, review}, index) => {
+            return (
+              <article key={index} className="testimonial">
           <div className="client__avatar">
-            <img src={AVTR1} alt="Avatar One" />
-            <h5 className='client__name'>Ryan Banks</h5>
-            <small className='client__review'>
-              Incididunt esse id dolore exercitation voluptate amet anim enim. Dolore ex deserunt aute est sunt. In culpa incididunt reprehenderit adipisicing eu ex duis esse ullamco officia. Ipsum reprehenderit pariatur amet aliquip veniam quis in.
-            </small>
+            <img src={avatar}/>
           </div>
+          <h5 className='client__name'>{name}</h5>
+            <small className='client__review'>{review}</small>
         </article>
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={AVTR1} alt="Avatar One" />
-            <h5 className='client__name'>Ryan Banks</h5>
-            <small className='client__review'>
-              Incididunt esse id dolore exercitation voluptate amet anim enim. Dolore ex deserunt aute est sunt. In culpa incididunt reprehenderit adipisicing eu ex duis esse ullamco officia. Ipsum reprehenderit pariatur amet aliquip veniam quis in.
-            </small>
-          </div>
-        </article>
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={AVTR1} alt="Avatar One" />
-            <h5 className='client__name'>Ryan Banks</h5>
-            <small className='client__review'>
-              Incididunt esse id dolore exercitation voluptate amet anim enim. Dolore ex deserunt aute est sunt. In culpa incididunt reprehenderit adipisicing eu ex duis esse ullamco officia. Ipsum reprehenderit pariatur amet aliquip veniam quis in.
-            </small>
-          </div>
-        </article>
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={AVTR1} alt="Avatar One" />
-            <h5 className='client__name'>Ryan Banks</h5>
-            <small className='client__review'>
-              Incididunt esse id dolore exercitation voluptate amet anim enim. Dolore ex deserunt aute est sunt. In culpa incididunt reprehenderit adipisicing eu ex duis esse ullamco officia. Ipsum reprehenderit pariatur amet aliquip veniam quis in.
-            </small>
-          </div>
-        </article>
+          )
+          })
+        }
       </div>
     </section>
   )
