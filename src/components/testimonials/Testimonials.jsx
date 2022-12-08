e38704471d458852242aed23e1558a1fbdf547ab
Brian <brian.lockerbie@gmail.com>
2022-12-08 10:52:58 -0800

    added swiper.js for testimonials

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 import './testimonials.css'
 import AVTR1 from '../../assets/avatar1.jpg'
 
+// import Swiper core and required modules
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
 const data = [
   {
     avatar: AVTR1,
@@ -10,17 +20,17 @@ const data = [
   },
   {
     avatar: AVTR1,
-    name: 'Scott Grove',
+    name: 'Marc Jacobs',
     review: 'Nulla non adipisicing amet ut anim. Ad laborum laboris veniam deserunt eiusmod. Dolor pariatur sit eiusmod adipisicing excepteur duis fugiat laborum occaecat cupidatat sit sint ex.'
   },
   {
     avatar: AVTR1,
-    name: 'Scott Grove',
+    name: 'Amber Dawn',
     review: 'Nulla non adipisicing amet ut anim. Ad laborum laboris veniam deserunt eiusmod. Dolor pariatur sit eiusmod adipisicing excepteur duis fugiat laborum occaecat cupidatat sit sint ex.'
   },
   {
     avatar: AVTR1,
-    name: 'Scott Grove',
+    name: 'Jamie E.',
     review: 'Nulla non adipisicing amet ut anim. Ad laborum laboris veniam deserunt eiusmod. Dolor pariatur sit eiusmod adipisicing excepteur duis fugiat laborum occaecat cupidatat sit sint ex.'
   },
 ]
@@ -31,21 +41,21 @@ const Testimonials = () => {
       <h5>Review from clients</h5>
       <h2>Testimonials</h2>
 
-      <div className="container testimonials__container">
+      <Swiper className="container testimonials__container">
         {
           data.map(({avatar, name, review}, index) => {
             return (
-              <article key={index} className="testimonial">
-          <div className="client__avatar">
-            <img src={avatar}/>
-          </div>
-          <h5 className='client__name'>{name}</h5>
-            <small className='client__review'>{review}</small>
-        </article>
+              <SwiperSlide key={index} className="testimonial">
+              <div className="client__avatar">
+                <img src={avatar}/>
+              </div>
+              <h5 className='client__name'>{name}</h5>
+              <small className='client__review'>{review}</small>
+              </SwiperSlide>
           )
           })
         }
-      </div>
+      </Swiper>
     </section>
   )
 }
