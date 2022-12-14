6283dc3f1df1a2f6a8962021ea62ffa608f8a7d9
Brian <brian.lockerbie@gmail.com>
2022-12-14 00:02:14 -0800

    removed header image

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import ME from '../../assets/brian-pro-pic.png'
+// import ME from '../../assets/brian-pro-pic.png'
 import HeaderSocial from './HeaderSocials'
 
 const Header = () => {
@@ -13,11 +13,9 @@ const Header = () => {
         <h5 className="text-light">Fullstack Developer</h5>
         <CTA />
         <HeaderSocial />
-
-        <div className="me">
+        {/* <div className="me">
           <img src={ME} alt="me" />
-        </div>
-
+        </div> */}
         <a href="#contact" className='scroll__down'>Scroll Down</a>
       </div>
     </header>
