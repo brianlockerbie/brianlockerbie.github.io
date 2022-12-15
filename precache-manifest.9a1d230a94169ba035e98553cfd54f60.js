e7aed6b11ff00f372711bcfc547c68e80fbff85a
Brian <brian.lockerbie@gmail.com>
2022-12-14 19:27:59 -0800

    Updates

---
+++ precache-manifest.9a1d230a94169ba035e98553cfd54f60.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "962807ef77558c07d1fb",
+    "url": "/static/css/main.2ca65250.chunk.css"
+  },
+  {
+    "revision": "962807ef77558c07d1fb",
+    "url": "/static/js/main.26bf84b3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2c1da4d69ba1add681c9",
+    "url": "/static/js/2.b45cb223.chunk.js"
+  },
+  {
+    "revision": "288c17cb9fdb3b48cbede9c61fae6338",
+    "url": "/static/media/BrianLockerbieResume2022.288c17cb.pdf"
+  },
+  {
+    "revision": "85a65087ae1382ca17dbd86abd03e533",
+    "url": "/static/media/brian-about.85a65087.png"
+  },
+  {
+    "revision": "a2f3bcdac96d83b365d694b98676cc20",
+    "url": "/static/media/portfolio1.a2f3bcda.png"
+  },
+  {
+    "revision": "91d11a126cf51d796066cae36f84bea9",
+    "url": "/static/media/portfolio2.91d11a12.png"
+  },
+  {
+    "revision": "72c7630daea4c19d4eaaaae23f9cc999",
+    "url": "/static/media/portfolio3.72c7630d.png"
+  },
+  {
+    "revision": "cebb6463105eb06903558bcbbb76b329",
+    "url": "/static/media/portfolio4.cebb6463.png"
+  },
+  {
+    "revision": "e421fdb256976b94db64aebffb70d75f",
+    "url": "/static/media/portfolio5.e421fdb2.png"
+  },
+  {
+    "revision": "8998403f9289bc1dd8a167bfff175a02",
+    "url": "/static/media/portfolio6.8998403f.png"
+  },
+  {
+    "revision": "648f2e757f34106ce2c9f6175a9abf43",
+    "url": "/index.html"
+  }
+];
