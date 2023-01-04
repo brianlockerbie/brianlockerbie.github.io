249740968999168949a7996d207809db33f8022e
Brian <brian.lockerbie@gmail.com>
2023-01-03 17:18:20 -0800

    added social links and changed icon

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import {BsLinkedin} from 'react-icons/bs'
 import {FaGithub} from 'react-icons/fa'
-import {FiDribbble} from 'react-icons/fi'
+import {IoLogoTwitter} from 'react-icons/io'
 
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://linkedin.com" target="_blank"><BsLinkedin/></a>
-        <a href="https://github.com" target="_blank"><FaGithub/></a>
-        <a href="https://dribbble.com" target="_blank"><FiDribbble/></a>
+        <a href="https://www.linkedin.com/in/brian-lockerbie" target="_blank"><BsLinkedin/></a>
+        <a href="https://github.com/brianlockerbie" target="_blank"><FaGithub/></a>
+        <a href="https://twitter.com/BrianLockerbie"><IoLogoTwitter/></a>
     </div>
   )
 }
